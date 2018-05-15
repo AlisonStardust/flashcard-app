72cd68c89deffcb4bc58fa18b53838f79c7ab7ba
alisonstardust <zakulisami@o2.pl>
2018-05-15 22:43:27 +0200

    colors and counter added

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function app () {
   const cardApiAddress = "https://gist.githubusercontent.com/vergilius/6d869a7448e405cb52d782120b77b82c/raw/e75dc7c19b918a9f0f5684595899dba2e5ad4f43/history-flashcards.json"
   let data;
   const cardDeck = document.querySelector('.cardDeck');
+  const counter = document.querySelector('.counter');
 
   function apirequest(apiAddress) {
     fetch(apiAddress)
@@ -12,9 +13,15 @@ function app () {
   }
 
   function getSepecific(responseAgain) {
+    counter.style.backgroundColor = "#7a8adb";
     let tempAnswer;
     data = responseAgain;
-    console.log(data[i]);
+    let counterHTML = `<div></div>`
+    counterHTML += `<svg class="sg-icon sg-icon--x24">
+                      <use xlink:href="#icon-student"></use>
+                    </svg> Cards left: ${data.length}`;
+    counter.innerHTML = counterHTML;
+    console.log(data);
     let deck = `<div></div>`
     deck += `
     <div class="sg-card sg-card--padding-large">
@@ -33,28 +40,32 @@ function app () {
     //console.log(data[i].answers[0].correct, answer[0])
     answer[0].addEventListener("click", function dupsko() {
       if (data[i].answers[0].correct === true) {
+        counter.style.backgroundColor = "#53cf92";
         console.log("yes!")
         //i++;
         data.shift(data[i]);
-        getSepecific(data);
+        setTimeout(() => getSepecific(data), 1000);
       } else {
+        counter.style.backgroundColor = "#ff796b";
         data.push(data[i]);
         data.shift(data[i]);
         console.log("dupa", data)
-        getSepecific(data);
+        setTimeout(() => getSepecific(data), 1000);
       }
     });
     answer[1].addEventListener("click", function dupsko() {
       if (data[i].answers[1].correct === true) {
+        counter.style.backgroundColor = "#53cf92";
         console.log("yes!")
         //i++;
         data.shift(data[i]);
-        getSepecific(data);
+        setTimeout(() => getSepecific(data), 1000);
       } else {
+        counter.style.backgroundColor = "#ff796b";
         data.push(data[i]);
         data.shift(data[i]);
         console.log("dupa", data)
-        getSepecific(data);
+        setTimeout(() => getSepecific(data), 1000);
       }
     });
   }
