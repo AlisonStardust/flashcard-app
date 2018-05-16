3973396ba3390668423c96848bd10eeb4a2a2f46
alisonstardust <zakulisami@o2.pl>
2018-05-16 22:48:31 +0200

    counter, card and styles updated

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 function app () {
   //all the DOM elements
   const cardDeck = document.querySelector('.cardDeck');
-  const counter = document.querySelector('.sg-box');
-  const logo = document.querySelector('.sg-logo');
-  const won = document.querySelector('.sg-text-bit');
+  const counter = document.querySelector('.counter-wrapper');
+  const endGame = document.querySelector('.heading');
   const result = document.querySelector('.result');
 
   let i = 0;
@@ -14,95 +13,115 @@ function app () {
   function apirequest(apiAddress) {
     fetch(apiAddress)
       .then(resp => resp.json())
-      .then(getSepecific)
+      .then(getSepecificData)
       .catch(error => console.log('Error: ', error));
   }
 
-  function getSepecific(responseAgain) {
+  function getSepecificData(responseAgain) {
+
     data = responseAgain;
-    counter.style.backgroundColor = "#7a8adb";
+    counter.style.backgroundColor = "#3a548d";
+
     //print cards and counter in HTML
-    let counterHTML = `<div></div>`
-    counterHTML += `<svg class="sg-icon sg-icon--x24">
+    let counterScript = `<div></div>`
+    counterScript += `<svg class="sg-icon sg-icon--x24">
                       <use xlink:href="#icon-student"></use>
                     </svg> Cards left: ${data.length}`;
-    counter.innerHTML = counterHTML;
-    console.log(data);
-    let deckHTML = `<div></div>`
-    deckHTML += `
-    <div class="sg-card sg-card--padding-large">
-          <svg class="sg-sticker">
-                <use class="sg-sticker__back" xlink:href="#icon-question"></use>
-                <use class="sg-sticker__front" xlink:href="#icon-question"></use>
-      </svg>
-      <div class="sg-card__hole">${data[i].question}</div>
-      <div class="sg-card__hole sg-card__hole--gray-secondary-lightest">${data[i].answers[0].answer}</div>
-      <div class="sg-card__hole sg-card__hole--gray-secondary-lightest">${data[i].answers[1].answer}</div>
-    </div>
-    `;
-    cardDeck.innerHTML = deckHTML;
-    const answer = document.querySelectorAll('.sg-card__hole--gray-secondary-lightest');
-    const ass = document.querySelector('.sg-card');
-    console.log(attempts)
-    answer[0].addEventListener("click", function dupsko() {
-      if (data[i].answers[0].correct === true) {
-        counter.style.backgroundColor = "#53cf92";
-        console.log("yes!")
-        data.shift(data[i]);
-        attempts++;
-        checknumber();
-        setTimeout(() => getSepecific(data), 1000);
-      } else {
-        counter.style.backgroundColor = "#ff796b";
-        data.push(data[i]);
-        data.shift(data[i]);
-        attempts++;
-        checknumber();
-        console.log("dupa", data)
-        setTimeout(() => getSepecific(data), 1000);
-      }
+    counter.innerHTML = counterScript;
+    
+    let deckScript = `<div></div>`
+    deckScript += `<div class="sg-card sg-card--padding-large">
+                    <svg class="sg-sticker">
+                      <use class="sg-sticker__back" xlink:href="#icon-question"></use>
+                      <use class="sg-sticker__front" xlink:href="#icon-question"></use>
+                    </svg>
+                    <div class="sg-card__hole">${data[i].question}
+                    </div>
+                    <div class="sg-card__hole sg-card__hole--gray-secondary-lightest">${data[i].answers[0].answer}
+                    </div>
+                    <div class="sg-card__hole sg-card__hole--gray-secondary-lightest">${data[i].answers[1].answer}
+                    </div>
+                  </div>
+                `;
+    cardDeck.innerHTML = deckScript;
+
+    const answers = document.querySelectorAll('.sg-card__hole--gray-secondary-lightest');
+
+    // function clickOnCardListeners() {
+    //   answers.forEach((answer) => {
+    //     answer.addEventListener("click", function(x) {
+
+    //     })
+    //   });
+    // }
+
+    answers[0].addEventListener("click", function checkUserAnswer() {
+        if (data[i].answers[0].correct === true) {
+          counter.style.backgroundColor = "#53cf92";
+          data.shift(data[i]);
+          attempts++;
+          checkDataLength();
+          setTimeout(() => getSepecificData(data), 1000);
+        } else {
+          counter.style.backgroundColor = "#ff796b";
+          data.push(data[i]);
+          data.shift(data[i]);
+          attempts++;
+          checkDataLength();
+          setTimeout(() => getSepecificData(data), 1000);
+        }
     });
-    answer[1].addEventListener("click", function dupsko() {
-      if (data[i].answers[1].correct === true) {
-        counter.style.backgroundColor = "#53cf92";
-        console.log("yes!")
-        data.shift(data[i]);
-        attempts++;
-        checknumber();
-        setTimeout(() => getSepecific(data), 1000);
-      } else {
-        counter.style.backgroundColor = "#ff796b";
-        data.push(data[i]);
-        data.shift(data[i]);
-        attempts++;
-        checknumber();
-        console.log("dupa", data)
-        setTimeout(() => getSepecific(data), 1000);
-      }
+    answers[1].addEventListener("click", function checkUserAnswer() {
+        if (data[i].answers[1].correct === true) {
+          counter.style.backgroundColor = "#53cf92";
+          data.shift(data[i]);
+          attempts++;
+          checkDataLength();
+          setTimeout(() => getSepecificData(data), 1000);
+        } else {
+          counter.style.backgroundColor = "#ff796b";
+          data.push(data[i]);
+          data.shift(data[i]);
+          attempts++;
+          checkDataLength();
+          setTimeout(() => getSepecificData(data), 1000);
+        }
     });
   }
 
-  function checknumber() {
+  function checkDataLength() {
     if (data.length === 0) {
-      won.style.display = "block";
-      checkResults();
+      endGame.style.display = "block";
+      printResults();
+      cardDeck.style.display = "none";
+      counter.style.display = "none";
     }
   }
 
-  function checkResults() {
-    result.style.display = "block";
+  function printResults() {
     let resultHTML = `<div></div>`
-      if (attempts <= 15) {
+      if (attempts <= 12) {
         resultHTML += `<h1 class="sg-text-bit sg-text-bit--small sg-text-bit--not-responsive">
-                      You did well!
+                      You did very well! Your attempts: ${attempts}
                       </h1>`;
         result.innerHTML = resultHTML;
-      } else if (attempts > 15) {
+      } else if (attempts > 12 && attempts <= 16) {
         resultHTML += `<h1 class="sg-text-bit sg-text-bit--small sg-text-bit--not-responsive">
-                      You did poor!
+                      You did well! Your attempts: ${attempts}
+                      </h1>`;
+        result.innerHTML = resultHTML;
+      } else if (attempts > 16 && attempts <= 20) {
+        resultHTML += `<h1 class="sg-text-bit sg-text-bit--small sg-text-bit--not-responsive">
+                      You did fine! Your attempts: ${attempts}
+                      </h1>`;
+        result.innerHTML = resultHTML;
+      } else if (attempts > 20) {
+        resultHTML += `<h1 class="sg-text-bit sg-text-bit--small sg-text-bit--not-responsive">
+                      You did poor! Your attempts: ${attempts}
                       </h1>`;
         result.innerHTML = resultHTML;
       }
+    result.style.display = "block";
   }
 
   apirequest(cardApiAddress);
